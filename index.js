e2e825efa54eda29e0293008240c71e4b4cea7cb
Satyaprakash Roy <149265635+SatyaT10@users.noreply.github.com>
2025-09-28 14:52:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// Connect to MongoDB
-
-let submissionsCollection;
-
-
-
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -28,18 +22,6 @@ app.get('/', (req, res) => {
 
 
 app.use('/api', router)
-
-app.get('/', async (req, res) => {
-  try {
-    const rows = await submissionsCollection.find().sort({ _id: -1 }).limit(100).toArray();
-    res.json({ ok: true, rows });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ ok: false, error: err.toString() });
-  }
-});
-
-
 
 
 app.listen(PORT, () => {
