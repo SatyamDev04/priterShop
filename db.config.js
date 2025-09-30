69e65f52c892f536f0b985e38adc302b4edafbcd
SatyamDev04 <dubeys867@gmail.com>
2025-09-30 18:31:59 +0530

    ewrfwefew

--- db.config.js
+++ db.config.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://satdad234_db_user:cUxL9H0ghM2Hf3s5@cluster0.wtkbbb6.mongodb.net/?printerShopDBretryWrites=true&w=majority&appName=Cluster0", {
+require('dotenv').config();
+ 
+// const MONGO_URI = process.env.MONGO_URI;
+MONGO_URI = "mongodb+srv://satdad234_db_user:cUxL9H0ghM2Hf3s5@cluster0.wtkbbb6.mongodb.net/printerShopDB?retryWrites=true&w=majority&appName=Cluster0"
+ 
+ 
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -9,4 +14,3 @@ mongoose.connect("mongodb+srv://satdad234_db_user:cUxL9H0ghM2Hf3s5@cluster0.wtkb
         console.error("❌ MongoDB connection error:", err);
         process.exit(1);
     });
-
